01770b0210c4a174eff90fb52cea4fb48a26d58f
GUNNYTHATIPAMULA <gunnysmartz@gmail.com>
2025-05-30 18:51:12 +0530

    Designed UI and Made Authentication For Marketing Manager

---
+++ sales-mgmt/src/Pages/DailyReminder.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from '../components/Header/Header'
+
+const DailyReminder = () => {
+  return (
+    <div>
+           <Header/>
+    <div className="flex w-full">
+          <h1>Dailncal</h1>
+       
+    </div>
+    
+    </div>
+  )
+}
+
+export default DailyReminder
