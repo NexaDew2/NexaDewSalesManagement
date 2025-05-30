6171c12df92ef5ab89829fe87a74aeb7da53b67f
GUNNYTHATIPAMULA <139752490+GUNNYTHATIPAMULA@users.noreply.github.com>
2025-05-30 20:01:53 +0530

    Designed UI and Made Authentication For Marketing Manager (#1)

--- sales-mgmt/src/App.jsx
+++ sales-mgmt/src/App.jsx
@@ -1,8 +1,28 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Pages/Home/Home';
+import MarketingManagerRegister from './Pages/MarketingManager/MarketingManagerRegister';
+import Login from './Pages/Login/Login';
+import AddNewLead from './Pages/AddNewLead';
+import DailyReminder from './Pages/DailyReminder';
+import Settings from './Pages/Settings';
+// import Header from './components/Header/Header';
+// import React from 'react';
 
-function App() {
-
+const App = () => {
   return (
     <>
+
+      <Router>
+        <Routes>
+
+          <Route path='/' element={<Home />} />
+          <Route path='/marketing-manager/register' element={<MarketingManagerRegister />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/AddNewLead' element={<AddNewLead />} />
+          <Route path='/DailyReminder' element={<DailyReminder />} />
+          <Route path='/Settings' element={<Settings />} />
+        </Routes>
+      </Router>
     </>
   )
 }
---
+++ sales-mgmt/src/Pages/AddNewLead.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../components/Header/Header'
+
+const AddNewLead = () => {
+  return (
+    <div>
+        <Header/>
+        <h1>AddNewLead</h1>
+    </div>
+  )
+}
+
+export default AddNewLead
